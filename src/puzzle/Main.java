92e1e747f7d87c4aa4df712414a3d63bbd4c2843
Erasmo Junkes <erasmojunkes@hotmail.com>
2021-05-04 21:32:04 -0300

    Remover os comandos repetidos de cath

--- src/puzzle/Main.java
+++ src/puzzle/Main.java
@@ -52,16 +52,7 @@ public class Main {
 
 			UIManager.setLookAndFeel(
 			        "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnsupportedLookAndFeelException e) {
+		} catch (ClassNotFoundException | UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
